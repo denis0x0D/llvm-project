e8f5d93dad94b75b05e3ccdb4ef4fa7e4fef16b4
Denis Khalikov <khalikov.denis@huawei.com>
2020-02-10 14:34:58 +0300

    Clean instrumentation pass

--- mlir/lib/Conversion/GPUToSPIRV/ConvertLaunchFuncToSPIRVCalls.cpp
+++ mlir/lib/Conversion/GPUToSPIRV/ConvertLaunchFuncToSPIRVCalls.cpp
@@ -28,17 +28,11 @@
 
 using namespace mlir;
 
-// To avoid name mangling, these are defined in the mini-runtime file.
-static constexpr const char *cuModuleLoadName = "mcuModuleLoad";
-static constexpr const char *cuModuleGetFunctionName = "mcuModuleGetFunction";
-static constexpr const char *cuLaunchKernelName = "mcuLaunchKernel";
-static constexpr const char *cuGetStreamHelperName = "mcuGetStreamHelper";
-static constexpr const char *cuStreamSynchronizeName = "mcuStreamSynchronize";
-static constexpr const char *kMcuMemHostRegister = "mcuMemHostRegister";
-static const char *kBufferRegister = "bufferRegister";
-
-static constexpr const char *kCubinAnnotation = "nvvm.cubin";
-static constexpr const char *kCubinStorageSuffix = "_cubin_cst";
+static constexpr const char *kSetEntryPoint = "setEntryPoint";
+static constexpr const char *kSetNumWorkGroups = "setNumWorkGroups";
+static constexpr const char *kSetBinaryShader = "setBinaryShader";
+static constexpr const char *kRunOnVulkan = "runOnVulkan";
+static constexpr const char *kSPIRV_BIN = "SPIRV_BIN";
 
 namespace {
 
@@ -52,73 +46,36 @@ private:
   }
 
   void initializeCachedTypes() {
-    const llvm::Module &module = llvmDialect->getLLVMModule();
     llvmVoidType = LLVM::LLVMType::getVoidTy(llvmDialect);
     llvmPointerType = LLVM::LLVMType::getInt8PtrTy(llvmDialect);
-    llvmPointerPointerType = llvmPointerType.getPointerTo();
-    llvmInt8Type = LLVM::LLVMType::getInt8Ty(llvmDialect);
     llvmInt32Type = LLVM::LLVMType::getInt32Ty(llvmDialect);
-    llvmInt64Type = LLVM::LLVMType::getInt64Ty(llvmDialect);
-    llvmIntPtrType = LLVM::LLVMType::getIntNTy(
-        llvmDialect, module.getDataLayout().getPointerSizeInBits());
   }
 
   LLVM::LLVMType getVoidType() { return llvmVoidType; }
-
   LLVM::LLVMType getPointerType() { return llvmPointerType; }
-
-  LLVM::LLVMType getPointerPointerType() { return llvmPointerPointerType; }
-
-  LLVM::LLVMType getInt8Type() { return llvmInt8Type; }
-
   LLVM::LLVMType getInt32Type() { return llvmInt32Type; }
 
-  LLVM::LLVMType getInt64Type() { return llvmInt64Type; }
-
-  LLVM::LLVMType getIntPtrType() {
-    const llvm::Module &module = getLLVMDialect()->getLLVMModule();
-    return LLVM::LLVMType::getIntNTy(
-        getLLVMDialect(), module.getDataLayout().getPointerSizeInBits());
-  }
-
-  LLVM::LLVMType getCUResultType() {
-    // This is declared as an enum in CUDA but helpers use i32.
-    return getInt32Type();
-  }
-
-  // Allocate a void pointer on the stack.
-  Value allocatePointer(OpBuilder &builder, Location loc) {
-    auto one = builder.create<LLVM::ConstantOp>(loc, getInt32Type(),
-                                                builder.getI32IntegerAttr(1));
-    return builder.create<LLVM::AllocaOp>(loc, getPointerPointerType(), one,
-                                          /*alignment=*/0);
-  }
-
-  void declareSPIRVFunctions(Location loc);
-  Value setupParamsArray(gpu::LaunchFuncOp launchOp, OpBuilder &builder);
+  void declareVulkanFunctions(Location loc);
   Value generateKernelNameConstant(StringRef name, Location loc,
                                    OpBuilder &builder);
   void translateGpuLaunchCalls(mlir::gpu::LaunchFuncOp launchOp);
+  LogicalResult generateBinaryShader(ModuleOp module,
+                                     std::vector<char> &binaryShader);
 
 public:
-  // Run the dialect converter on the module.
   void runOnModule() override {
-    // Cache the LLVMDialect for the current module.
     llvmDialect = getContext().getRegisteredDialect<LLVM::LLVMDialect>();
-    // Cache the used LLVM types.
     initializeCachedTypes();
 
     getModule().walk([this](mlir::gpu::LaunchFuncOp op) {
       translateGpuLaunchCalls(op);
     });
 
-    // GPU kernel modules are no longer necessary since we have a global
-    // constant with the CUBIN data.
+    // Erase `gpu::GPUModuleOp` and `spirv::Module` operations.
     for (auto m :
          llvm::make_early_inc_range(getModule().getOps<gpu::GPUModuleOp>()))
       m.erase();
 
-    // Erase spirv::Module operations.
     for (auto spirvModule :
          llvm::make_early_inc_range(getModule().getOps<spirv::ModuleOp>()))
       spirvModule.erase();
@@ -128,31 +85,42 @@ private:
   LLVM::LLVMDialect *llvmDialect;
   LLVM::LLVMType llvmVoidType;
   LLVM::LLVMType llvmPointerType;
-  LLVM::LLVMType llvmPointerPointerType;
-  LLVM::LLVMType llvmInt8Type;
   LLVM::LLVMType llvmInt32Type;
-  LLVM::LLVMType llvmInt64Type;
-  LLVM::LLVMType llvmIntPtrType;
 };
-
 } // anonymous namespace
 
-// Adds declarations for the needed helper functions from the CUDA wrapper.
-// The types in comments give the actual types expected/returned but the API
-// uses void pointers. This is fine as they have the same linkage in C.
-void GpuLaunchFuncToSPIRVCallsPass::declareSPIRVFunctions(Location loc) {
+void GpuLaunchFuncToSPIRVCallsPass::declareVulkanFunctions(Location loc) {
   ModuleOp module = getModule();
   OpBuilder builder(module.getBody()->getTerminator());
-  if (!module.lookupSymbol(cuModuleLoadName)) {
+
+  if (!module.lookupSymbol(kSetEntryPoint)) {
     builder.create<LLVM::LLVMFuncOp>(
-        loc, cuModuleLoadName,
+        loc, kSetEntryPoint,
+        LLVM::LLVMType::getFunctionTy(llvmVoidType, {llvmPointerType},
+                                      /*isVarArg=*/false));
+  }
+
+  if (!module.lookupSymbol(kSetNumWorkGroups)) {
+    builder.create<LLVM::LLVMFuncOp>(
+        loc, kSetNumWorkGroups,
         LLVM::LLVMType::getFunctionTy(
-            getCUResultType(),
-            {
-                getPointerPointerType(), /* CUmodule *module */
-                getPointerType()         /* void *cubin */
-            },
+            llvmVoidType, {llvmInt32Type, llvmInt32Type, llvmInt32Type},
             /*isVarArg=*/false));
+  }
+
+  if (!module.lookupSymbol(kSetBinaryShader)) {
+    builder.create<LLVM::LLVMFuncOp>(
+        loc, kSetBinaryShader,
+        LLVM::LLVMType::getFunctionTy(llvmVoidType,
+                                      {llvmPointerType, llvmInt32Type},
+                                      /*isVarArg=*/false));
+  }
+
+  if (!module.lookupSymbol(kRunOnVulkan)) {
+    builder.create<LLVM::LLVMFuncOp>(
+        loc, kRunOnVulkan,
+        LLVM::LLVMType::getFunctionTy(llvmVoidType, {},
+                                      /*isVarArg=*/false));
   }
 }
 
@@ -168,33 +136,42 @@ Value GpuLaunchFuncToSPIRVCallsPass::generateKernelNameConstant(
       LLVM::Linkage::Internal, llvmDialect);
 }
 
-void GpuLaunchFuncToSPIRVCallsPass::translateGpuLaunchCalls(
-    mlir::gpu::LaunchFuncOp launchOp) {
-  ModuleOp module = getModule();
-  OpBuilder funcBuilder(module.getBody()->getTerminator());
-  OpBuilder builder(launchOp);
-
-  Location loc = launchOp.getLoc();
-
+LogicalResult GpuLaunchFuncToSPIRVCallsPass::generateBinaryShader(
+    ModuleOp module, std::vector<char> &binaryShader) {
   bool done = false;
   SmallVector<uint32_t, 0> binary;
   for (auto spirvModule : module.getOps<spirv::ModuleOp>()) {
     if (done) {
-      return signalPassFailure();
+      return failure();
     }
     done = true;
     if (failed(spirv::serialize(spirvModule, binary))) {
-      return signalPassFailure();
+      return failure();
     }
   }
-
-  std::vector<char> binaryShader;
   binaryShader.resize(binary.size() * sizeof(uint32_t));
   std::memcpy(binaryShader.data(), reinterpret_cast<char *>(binary.data()),
               binaryShader.size());
 
+  return success();
+}
+
+void GpuLaunchFuncToSPIRVCallsPass::translateGpuLaunchCalls(
+    mlir::gpu::LaunchFuncOp launchOp) {
+  ModuleOp module = getModule();
+  OpBuilder builder(launchOp);
+  Location loc = launchOp.getLoc();
+  
+  declareVulkanFunctions(loc);
+  std::vector<char> binaryShader;
+
+  if (failed(GpuLaunchFuncToSPIRVCallsPass::generateBinaryShader(
+          module, binaryShader))) {
+    return signalPassFailure();
+  }
+
   Value ptrToExec = LLVM::createGlobalString(
-      loc, builder, "executable",
+      loc, builder, kSPIRV_BIN,
       StringRef(binaryShader.data(), binaryShader.size()),
       LLVM::Linkage::Internal, getLLVMDialect());
 
@@ -203,21 +180,9 @@ void GpuLaunchFuncToSPIRVCallsPass::translateGpuLaunchCalls(
 
   auto kernelName = generateKernelNameConstant(launchOp.kernel(), loc, builder);
 
-  // FIXME: Add size value.
-  funcBuilder.create<LLVM::LLVMFuncOp>(
-      loc, "setEntryPoint",
-      LLVM::LLVMType::getFunctionTy(llvmVoidType, {llvmPointerType},
-                                    /*isVarArg=*/false));
-
   builder.create<LLVM::CallOp>(loc, ArrayRef<Type>{llvmVoidType},
-                               builder.getSymbolRefAttr("setEntryPoint"),
+                               builder.getSymbolRefAttr(kSetEntryPoint),
                                ArrayRef<Value>{kernelName});
-
-  funcBuilder.create<LLVM::LLVMFuncOp>(
-      loc, "setNumWorkGroups",
-      LLVM::LLVMType::getFunctionTy(
-          llvmVoidType, {llvmInt32Type, llvmInt32Type, llvmInt32Type},
-          /*isVarArg=*/false));
 
   Value x = builder.create<LLVM::ConstantOp>(
       loc, getInt32Type(), builder.getI32IntegerAttr(launchOp.getOperand(0)));
@@ -227,31 +192,16 @@ void GpuLaunchFuncToSPIRVCallsPass::translateGpuLaunchCalls(
       loc, getInt32Type(), builder.getI32IntegerAttr(launchOp.getOperand(2)));
 
   builder.create<LLVM::CallOp>(loc, ArrayRef<Type>{llvmVoidType},
-                               builder.getSymbolRefAttr("setNumWorkGroups"),
+                               builder.getSymbolRefAttr(kSetNumWorkGroups),
                                ArrayRef<Value>{x, y, z});
 
-  // FIXME: Add size value.
-  funcBuilder.create<LLVM::LLVMFuncOp>(
-      loc, "setBinaryShader",
-      LLVM::LLVMType::getFunctionTy(llvmVoidType,
-                                    {llvmPointerType, llvmInt32Type},
-                                    /*isVarArg=*/false));
-
   builder.create<LLVM::CallOp>(loc, ArrayRef<Type>{llvmVoidType},
-                               builder.getSymbolRefAttr("setBinaryShader"),
+                               builder.getSymbolRefAttr(kSetBinaryShader),
                                ArrayRef<Value>{ptrToExec, binarySize});
 
-  funcBuilder.create<LLVM::LLVMFuncOp>(
-      loc, "runOnVulkan",
-      LLVM::LLVMType::getFunctionTy(llvmVoidType, {},
-                                    /*isVarArg=*/false));
-
   builder.create<LLVM::CallOp>(loc, ArrayRef<Type>{llvmVoidType},
-                               builder.getSymbolRefAttr("runOnVulkan"),
+                               builder.getSymbolRefAttr(kRunOnVulkan),
                                ArrayRef<Value>{});
-
-
-
   launchOp.erase();
 }
 
