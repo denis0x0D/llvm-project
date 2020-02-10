76b423f4e0846ffe6b7440bcde53b2df61d113a6
Denis Khalikov <khalikov.denis@huawei.com>
2020-02-10 14:53:09 +0300

    More clean up

--- mlir/lib/Conversion/GPUToSPIRV/ConvertLaunchFuncToSPIRVCalls.cpp
+++ mlir/lib/Conversion/GPUToSPIRV/ConvertLaunchFuncToSPIRVCalls.cpp
@@ -28,11 +28,11 @@
 
 using namespace mlir;
 
+static constexpr const char *kSetBinaryShader = "setBinaryShader";
 static constexpr const char *kSetEntryPoint = "setEntryPoint";
 static constexpr const char *kSetNumWorkGroups = "setNumWorkGroups";
-static constexpr const char *kSetBinaryShader = "setBinaryShader";
 static constexpr const char *kRunOnVulkan = "runOnVulkan";
-static constexpr const char *kSPIRV_BIN = "SPIRV_BIN";
+static constexpr const char *kSPIRVBinary = "SPIRV_BIN";
 
 namespace {
 
@@ -72,9 +72,9 @@ public:
     });
 
     // Erase `gpu::GPUModuleOp` and `spirv::Module` operations.
-    for (auto m :
+    for (auto gpuModule :
          llvm::make_early_inc_range(getModule().getOps<gpu::GPUModuleOp>()))
-      m.erase();
+      gpuModule.erase();
 
     for (auto spirvModule :
          llvm::make_early_inc_range(getModule().getOps<spirv::ModuleOp>()))
@@ -87,6 +87,7 @@ private:
   LLVM::LLVMType llvmPointerType;
   LLVM::LLVMType llvmInt32Type;
 };
+
 } // anonymous namespace
 
 void GpuLaunchFuncToSPIRVCallsPass::declareVulkanFunctions(Location loc) {
@@ -96,7 +97,7 @@ void GpuLaunchFuncToSPIRVCallsPass::declareVulkanFunctions(Location loc) {
   if (!module.lookupSymbol(kSetEntryPoint)) {
     builder.create<LLVM::LLVMFuncOp>(
         loc, kSetEntryPoint,
-        LLVM::LLVMType::getFunctionTy(llvmVoidType, {llvmPointerType},
+        LLVM::LLVMType::getFunctionTy(getVoidType(), {getPointerType()},
                                       /*isVarArg=*/false));
   }
 
@@ -104,22 +105,22 @@ void GpuLaunchFuncToSPIRVCallsPass::declareVulkanFunctions(Location loc) {
     builder.create<LLVM::LLVMFuncOp>(
         loc, kSetNumWorkGroups,
         LLVM::LLVMType::getFunctionTy(
-            llvmVoidType, {llvmInt32Type, llvmInt32Type, llvmInt32Type},
+            getVoidType(), {getInt32Type(), getInt32Type(), getInt32Type()},
             /*isVarArg=*/false));
   }
 
   if (!module.lookupSymbol(kSetBinaryShader)) {
     builder.create<LLVM::LLVMFuncOp>(
         loc, kSetBinaryShader,
-        LLVM::LLVMType::getFunctionTy(llvmVoidType,
-                                      {llvmPointerType, llvmInt32Type},
+        LLVM::LLVMType::getFunctionTy(getVoidType(),
+                                      {getPointerType(), getInt32Type()},
                                       /*isVarArg=*/false));
   }
 
   if (!module.lookupSymbol(kRunOnVulkan)) {
     builder.create<LLVM::LLVMFuncOp>(
         loc, kRunOnVulkan,
-        LLVM::LLVMType::getFunctionTy(llvmVoidType, {},
+        LLVM::LLVMType::getFunctionTy(getVoidType(), {},
                                       /*isVarArg=*/false));
   }
 }
@@ -149,10 +150,10 @@ LogicalResult GpuLaunchFuncToSPIRVCallsPass::generateBinaryShader(
       return failure();
     }
   }
+
   binaryShader.resize(binary.size() * sizeof(uint32_t));
   std::memcpy(binaryShader.data(), reinterpret_cast<char *>(binary.data()),
               binaryShader.size());
-
   return success();
 }
 
@@ -163,24 +164,25 @@ void GpuLaunchFuncToSPIRVCallsPass::translateGpuLaunchCalls(
   Location loc = launchOp.getLoc();
   
   declareVulkanFunctions(loc);
-  std::vector<char> binaryShader;
 
-  if (failed(GpuLaunchFuncToSPIRVCallsPass::generateBinaryShader(
-          module, binaryShader))) {
+  std::vector<char> binary;
+  if (failed(GpuLaunchFuncToSPIRVCallsPass::generateBinaryShader(module,
+                                                                 binary))) {
     return signalPassFailure();
   }
 
-  Value ptrToExec = LLVM::createGlobalString(
-      loc, builder, kSPIRV_BIN,
-      StringRef(binaryShader.data(), binaryShader.size()),
+  Value ptrToSPIRVBinary = LLVM::createGlobalString(
+      loc, builder, kSPIRVBinary, StringRef(binary.data(), binary.size()),
       LLVM::Linkage::Internal, getLLVMDialect());
+  Value binarySize = builder.create<LLVM::ConstantOp>(
+      loc, getInt32Type(), builder.getI32IntegerAttr(binary.size()));
+  builder.create<LLVM::CallOp>(loc, ArrayRef<Type>{getVoidType()},
+                               builder.getSymbolRefAttr(kSetBinaryShader),
+                               ArrayRef<Value>{ptrToSPIRVBinary, binarySize});
 
-  auto binarySize = builder.create<LLVM::ConstantOp>(
-      loc, getInt32Type(), builder.getI32IntegerAttr(binaryShader.size()));
-
-  auto kernelName = generateKernelNameConstant(launchOp.kernel(), loc, builder);
-
-  builder.create<LLVM::CallOp>(loc, ArrayRef<Type>{llvmVoidType},
+  Value kernelName =
+      generateKernelNameConstant(launchOp.kernel(), loc, builder);
+  builder.create<LLVM::CallOp>(loc, ArrayRef<Type>{getVoidType()},
                                builder.getSymbolRefAttr(kSetEntryPoint),
                                ArrayRef<Value>{kernelName});
 
@@ -190,16 +192,10 @@ void GpuLaunchFuncToSPIRVCallsPass::translateGpuLaunchCalls(
       loc, getInt32Type(), builder.getI32IntegerAttr(launchOp.getOperand(1)));
   Value z = builder.create<LLVM::ConstantOp>(
       loc, getInt32Type(), builder.getI32IntegerAttr(launchOp.getOperand(2)));
-
-  builder.create<LLVM::CallOp>(loc, ArrayRef<Type>{llvmVoidType},
+  builder.create<LLVM::CallOp>(loc, ArrayRef<Type>{getVoidType()},
                                builder.getSymbolRefAttr(kSetNumWorkGroups),
                                ArrayRef<Value>{x, y, z});
-
-  builder.create<LLVM::CallOp>(loc, ArrayRef<Type>{llvmVoidType},
-                               builder.getSymbolRefAttr(kSetBinaryShader),
-                               ArrayRef<Value>{ptrToExec, binarySize});
-
-  builder.create<LLVM::CallOp>(loc, ArrayRef<Type>{llvmVoidType},
+  builder.create<LLVM::CallOp>(loc, ArrayRef<Type>{getVoidType()},
                                builder.getSymbolRefAttr(kRunOnVulkan),
                                ArrayRef<Value>{});
   launchOp.erase();
